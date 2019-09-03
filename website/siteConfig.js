dc38b7e891df47cf78fdfeb8280de1b1e1a5df35
daminort <daminort@gmail.com>
2019-09-03 13:32:42 +0300

    Changed setting

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -18,7 +18,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'daminort.github.io',
+  projectName: 'data-caster-docs',
   organizationName: 'daminort',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
